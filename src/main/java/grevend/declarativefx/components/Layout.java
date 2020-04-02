eb147eb352906766e306ad0a7743d98dfdb3b28a
grevend <grevend@users.noreply.github.com>
2020-04-02 22:18:52 +0200

    Add support for TableView, TreeView and ListView.

--- src/main/java/grevend/declarativefx/components/Layout.java
+++ src/main/java/grevend/declarativefx/components/Layout.java
@@ -25,19 +25,28 @@
 package grevend.declarativefx.components;
 
 import grevend.declarativefx.Component;
+import grevend.declarativefx.properties.Lifecycle;
 import grevend.declarativefx.util.BindableValue;
+import grevend.declarativefx.util.GridBuilder;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
 import javafx.scene.Node;
-import javafx.scene.control.ScrollPane;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import static grevend.declarativefx.components.Compat.FX;
 import static grevend.declarativefx.components.Compat.FXContainer;
@@ -75,7 +84,7 @@ public class Layout {
     public static @NotNull FX<ImageView> Image(@NotNull String image, @NotNull double[] imgSize) {
         return FX(new ImageView(new Image(image, imgSize[0], imgSize[1], true, true)));
     }
-    
+
     @SafeVarargs
     public static @NotNull FXContainer<HBox> HBox(@NotNull Component<? extends Node>... components) {
         return FXContainer(new HBox(), components);
@@ -92,6 +101,21 @@ public class Layout {
 
     public static @NotNull FXContainer<VBox> VBox(@NotNull Collection<Component<? extends Node>> components) {
         return FXContainer(new VBox(), components);
+    }
+
+    @SafeVarargs
+    public static @NotNull <N extends Node> FX<ListView<N>> ListView(@NotNull Component<N>... components) {
+        var listView = new ListView<N>();
+        for (Component<N> component : components) {
+            listView.getItems().add(component.construct());
+        }
+        return FX(listView);
+    }
+
+    public static @NotNull <N extends Node> FX<ListView<N>> ListView(@NotNull Collection<Component<N>> components) {
+        var listView = new ListView<N>();
+        listView.getItems().addAll(components.stream().map(Lifecycle::construct).collect(Collectors.toList()));
+        return FX(listView);
     }
 
     public static @NotNull FX<ScrollPane> ScrollPane(@NotNull Component<? extends Node> component) {
@@ -120,6 +144,106 @@ public class Layout {
                 rightComponent != null ? rightComponent.construct() : null,
                 bottomComponent != null ? bottomComponent.construct() : null,
                 leftComponent != null ? leftComponent.construct() : null));
+    }
+
+    public static @NotNull FX<GridPane> GridPane(@NotNull Consumer<GridBuilder> builder) {
+        var gridPane = new GridPane();
+        var gridBuilder = new GridBuilder();
+        builder.accept(gridBuilder);
+        gridBuilder.getComponents().forEach(component -> {
+            gridPane.add(component.getA().construct(), component.getB(), component.getC());
+        });
+        return FX(gridPane);
+    }
+
+    public static @NotNull <C extends Node> FX<GridPane> GridPane(@NotNull Collection<Component<C>> components,
+                                                                  @NotNull BiConsumer<Collection<Component<C>>, GridBuilder> builder) {
+        var gridPane = new GridPane();
+        var gridBuilder = new GridBuilder();
+        builder.accept(components, gridBuilder);
+        gridBuilder.getComponents().forEach(component -> {
+            gridPane.add(component.getA().construct(), component.getB(), component.getC());
+        });
+        return FX(gridPane);
+    }
+
+    public static @NotNull TreeItem<String> TreeItem(@NotNull String item) {
+        return new TreeItem<>(item);
+    }
+
+    public static @NotNull TreeItem<String> TreeItem(@NotNull String item, boolean expanded) {
+        var treeItem = new TreeItem<>(item);
+        treeItem.setExpanded(expanded);
+        return treeItem;
+    }
+
+    @SafeVarargs
+    public static @NotNull TreeItem<String> TreeItem(@NotNull String item, boolean expanded,
+                                                     @NotNull TreeItem<String>... items) {
+        var treeItem = new TreeItem<>(item);
+        treeItem.setExpanded(expanded);
+        treeItem.getChildren().addAll(Arrays.asList(items));
+        return treeItem;
+    }
+
+    public static @NotNull TreeItem<String> TreeItem(@NotNull String item, boolean expanded,
+                                                     @NotNull Collection<TreeItem<String>> items) {
+        var treeItem = new TreeItem<>(item);
+        treeItem.setExpanded(expanded);
+        treeItem.getChildren().addAll(items);
+        return treeItem;
+    }
+
+    @SafeVarargs
+    public static @NotNull TreeItem<String> TreeItem(@NotNull String item, @NotNull TreeItem<String>... items) {
+        var treeItem = new TreeItem<>(item);
+        treeItem.getChildren().addAll(Arrays.asList(items));
+        return treeItem;
+    }
+
+    public static @NotNull TreeItem<String> TreeItem(@NotNull String item,
+                                                     @NotNull Collection<TreeItem<String>> items) {
+        var treeItem = new TreeItem<>(item);
+        treeItem.getChildren().addAll(items);
+        return treeItem;
+    }
+
+    public static @NotNull FX<TreeView<String>> TreeView(@NotNull TreeItem<String> root) {
+        return FX(new TreeView<>(root));
+    }
+
+    public static @NotNull FX<TableView<String[]>> TableView(@NotNull Collection<String[]> row,
+                                                             @NotNull String... columns) {
+        TableView<String[]> tableView = new TableView<>();
+        var index = 0;
+        for (String column : columns) {
+            TableColumn<String[], String> tableColumn = new TableColumn<>(column);
+            int finalIndex = index;
+            tableColumn.setCellValueFactory(p -> new SimpleObjectProperty<>(p.getValue()[finalIndex]));
+            tableView.getColumns().add(tableColumn);
+            index++;
+        }
+        tableView.setItems(FXCollections.observableArrayList(row));
+        return new FX<>(tableView);
+    }
+
+    public static @NotNull FX<TableView<String[]>> TableView(@NotNull Collection<String[]> row,
+                                                             @NotNull String[] columns, @NotNull double[] minWidth) {
+        if (columns.length != minWidth.length) {
+            throw new IllegalArgumentException("Amount of columns does not match given amount of widths.");
+        }
+        TableView<String[]> tableView = new TableView<>();
+        var index = 0;
+        for (String column : columns) {
+            TableColumn<String[], String> tableColumn = new TableColumn<>(column);
+            int finalIndex = index;
+            tableColumn.setCellValueFactory(p -> new SimpleObjectProperty<>(p.getValue()[finalIndex]));
+            tableColumn.setMinWidth(minWidth[finalIndex]);
+            tableView.getColumns().add(tableColumn);
+            index++;
+        }
+        tableView.setItems(FXCollections.observableArrayList(row));
+        return new FX<>(tableView);
     }
 
 }
