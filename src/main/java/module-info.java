926e9826015c8aad486ee62fd64266d9d15825fa
grevend <grevend@users.noreply.github.com>
2020-04-02 10:41:59 +0200

    Add java module support.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module DeclarativeFX {
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires org.jetbrains.annotations;
+
+    exports grevend.declarativefx;
+    exports grevend.declarativefx.components;
+    exports grevend.declarativefx.properties;
+    exports grevend.declarativefx.util;
+}
